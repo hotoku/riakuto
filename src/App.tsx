93e96ce4e37700b294e8d7e90dfa0f67416dbf0c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-10-16 10:21:35 +0900

    copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,14 @@
-import { Component, ReactElement } from "react";
-import { Button, Card, Statistic, Icon } from "semantic-ui-react";
-import "./App.css";
+import { VFC } from 'react';
+import Counter from './Counter';
+import './App.css';
 
-const LIMIT = 5;
-type State = { timeLeft: number };
+const App: VFC = () => (
+  <div className="container">
+    <header>
+      <h1>カウンター</h1>
+    </header>
+    <Counter />
+  </div>
+);
 
-class App extends Component<unknown, State> {
-  timerId: NodeJS.Timer | null = null;
-
-  constructor(props: unknown) {
-    super(props);
-    this.state = { timeLeft: LIMIT };
-  }
-
-  componentDidMount = (): void => {
-    this.timerId = setInterval(this.tick, 1000);
-  };
-
-  componentDidUpdate = (): void => {
-    const { timeLeft } = this.state;
-    if (timeLeft === 0) this.reset();
-  };
-
-  componentWillUnmount = (): void => {
-    if (this.timerId) clearInterval(this.timerId);
-  };
-
-  tick = (): void =>
-    this.setState((prevState) => ({ timeLeft: prevState.timeLeft - 1 }));
-
-  reset(): void {
-    this.setState({ timeLeft: LIMIT });
-  }
-
-  increment(): void {
-    this.setState((state) => ({ timeLeft: state.timeLeft + 1 }));
-  }
-
-  render(): ReactElement {
-    const { timeLeft } = this.state;
-    return (
-      <div className="container">
-        <header>
-          {" "}
-          <h1>カウンター</h1>
-        </header>
-        <Card>
-          <Statistic className="number-board">
-            <Statistic.Label>time</Statistic.Label>
-            <Statistic.Value>{timeLeft}</Statistic.Value>
-          </Statistic>
-          <Card.Content>
-            <Button color="red" onClick={() => this.reset()}>
-              <Icon name="redo" />
-              Reset
-            </Button>
-          </Card.Content>
-        </Card>
-      </div>
-    );
-  }
-}
 export default App;
